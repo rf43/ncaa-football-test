981dade54d6c74f96f2f0d5f0c0c557b3336adde
CursedFunction <cursedfunction@gmail.com>
2024-04-03 22:23:18 -0400

    Init project

---
+++ ktor-football/src/main/kotlin/io/cursedfunction/plugins/Routing.kt
@@ -0,0 +1,23 @@
+package io.cursedfunction.plugins
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.plugins.statuspages.*
+
+fun Application.configureRouting() {
+    install(StatusPages) {
+        exception<Throwable> { call, cause ->
+            call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
+        }
+    }
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        // Static plugin. Try to access `/static/index.html`
+        static("/static") {
+            resources("static")
+        }
+    }
+}
